efea229a5940285fe21f8fc099876e898486e686
Stefan Hauschildt <stefan.h@uschildt.de>
2017-12-17 19:09:27 +0100

    lombok - introduce lombok to project

--- src/main/java/freya/fitness/controller/CourseDetailDto.java
+++ src/main/java/freya/fitness/controller/CourseDetailDto.java
@@ -3,14 +3,17 @@ package freya.fitness.controller;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import freya.fitness.domain.Course;
 import freya.fitness.domain.CourseType;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Data
 public class CourseDetailDto {
 
   private Long id;
   private CourseType type;
+  @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
   private LocalDateTime start;
   private Long minutes;
   private String instructor;
@@ -23,54 +26,5 @@ public class CourseDetailDto {
     this.minutes = course.getMinutes();
     this.instructor = course.getInstructor();
     this.attendees = course.getAttendees();
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public CourseType getType() {
-    return type;
-  }
-
-  public void setType(CourseType type) {
-    this.type = type;
-  }
-
-  @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-  public LocalDateTime getStart() {
-    return start;
-  }
-
-  public void setStart(LocalDateTime start) {
-    this.start = start;
-  }
-
-  public Long getMinutes() {
-    return minutes;
-  }
-
-  public void setMinutes(Long minutes) {
-    this.minutes = minutes;
-  }
-
-  public String getInstructor() {
-    return instructor;
-  }
-
-  public void setInstructor(String instructor) {
-    this.instructor = instructor;
-  }
-
-  public List<String> getAttendees() {
-    return attendees;
-  }
-
-  public void setAttendees(List<String> attendees) {
-    this.attendees = attendees;
   }
 }
