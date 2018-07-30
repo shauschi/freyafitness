e62ae2888fd77fe6dd4dec6de53f0306d0e865cf
Stefan Hauschildt <stefan.h@uschildt.de>
2018-07-30 19:20:03 +0200

    - use expansion panels for courses and contact card

--- src/main/webapp/components/contact/ContactCard.jsx
+++ src/main/webapp/components/contact/ContactCard.jsx
@@ -1,39 +1,23 @@
 'use strict';
 import React, {Component} from 'react';
-import classnames from 'classnames';
-import {withStyles} from '@material-ui/core/styles';
 import {bindActionCreators} from 'redux';
 import compose from 'recompose/compose';
 import {connect} from 'react-redux';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
-import Collapse from '@material-ui/core/Collapse';
-import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import Grid from '@material-ui/core/Grid';
 import {GridButtonControl, GridInputControl, GridTextControl} from './../../components/general';
 import {loginDataChanged, sendContact} from '../../model/contact';
 import {ValidationGroup, Validators} from './../general/validation';
 import {IconExpandMore} from './../../utils/Icons';
 
-const styles = theme => ({
-  expand: {
-    transform: 'rotate(0deg)',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-    marginLeft: 'auto',
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)',
-  }
-});
 
 class ContactCard extends Component {
 
   state = {
-    autoFilled: false,
-    expanded: false
+    autoFilled: false
   };
 
   constructor(props) {
@@ -46,10 +30,6 @@ class ContactCard extends Component {
       loginDataChanged('email', props.user.email);
     }
   }
-
-  handleExpandClick = () => {
-    this.setState(state => ({expanded: !state.expanded}));
-  };
 
   validateForm = () => {
     const result = this.validationGroup.validate();
@@ -82,86 +62,70 @@ class ContactCard extends Component {
   };
 
   render() {
-    const {contact, classes, actions} = this.props;
+    const {contact, actions} = this.props;
     const {loginDataChanged} = actions;
     const {data, pending, errorMessage} = contact;
 
     return (
       <Grid item xs={12} sm={8} md={5}>
-        <Card>
-          <CardHeader title='Kontakt'/>
-          <CardContent>
+        <ExpansionPanel>
+          <ExpansionPanelSummary expandIcon={<IconExpandMore/>}>
+            <div>
+              <Typography variant='subheading'>Kontakt</Typography>
+              <Typography>Du hast Lust auf ein Probetraining oder möchtest Freya eine Nachricht schreiben? Dann los:</Typography>
+            </div>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
             <Grid container spacing={16} justify="center" style={{width: '100%', margin: '0px'}}>
-              <GridTextControl
-                xs={10}
-                text={'Du hast Lust auf ein Probetraining oder möchtest Freya eine Nachricht schreiben? Dann los:'}/>
-              <Grid item xs={2}>
-                <IconButton
-                  className={classnames(classes.expand, {
-                    [classes.expandOpen]: this.state.expanded,
-                  })}
-                  onClick={this.handleExpandClick}
-                  aria-expanded={this.state.expanded}
-                  aria-label="Show more"
-                >
-                  <IconExpandMore/>
-                </IconButton>
-              </Grid>
+              <ValidationGroup ref={this.setValidation}>
+                <GridInputControl
+                  id='firstname'
+                  label='Vorname'
+                  xs={6}
+                  value={data.firstname}
+                  validators={[Validators.notEmpty('Bitte gib deinen Vornamen ein')]}
+                  onChange={loginDataChanged}/>
+                <GridInputControl
+                  id='lastname'
+                  label='Nachname'
+                  xs={6}
+                  value={data.lastname}
+                  validators={[Validators.notEmpty('Bitte gib deinen Nachnamen ein')]}
+                  onChange={loginDataChanged}/>
+                <GridInputControl
+                  id='telephone'
+                  label='Telefon'
+                  value={data.telephone}
+                  validators={[this.validateContact(Validators.telephone())]}
+                  onChange={loginDataChanged}/>
+                <GridInputControl
+                  id='email'
+                  label='E-Mail'
+                  value={data.email}
+                  validators={[this.validateContact(Validators.email())]}
+                  onChange={loginDataChanged}/>
+                <GridInputControl
+                  id='subject'
+                  label='Betreff'
+                  value={data.subject}
+                  validators={[Validators.notEmpty('Bitte gib einen Betreff an')]}
+                  onChange={loginDataChanged}/>
+                <GridInputControl
+                  id='message'
+                  label='Nachricht'
+                  value={data.message}
+                  multiline
+                  validators={[Validators.notEmpty('Bitte trage hier deine Nachricht ein')]}
+                  onChange={loginDataChanged}/>
+                {errorMessage ? <GridTextControl text={errorMessage} error={true}/> : undefined}
+                <GridButtonControl
+                  pending={pending}
+                  label='absenden'
+                  onClick={this.doSendContact}/>
+              </ValidationGroup>
             </Grid>
-          </CardContent>
-          <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
-            <CardContent>
-              <Grid container spacing={16} justify="center" style={{width: '100%', margin: '0px'}}>
-                <ValidationGroup ref={this.setValidation}>
-                  <GridInputControl
-                    id='firstname'
-                    label='Vorname'
-                    xs={6}
-                    value={data.firstname}
-                    validators={[Validators.notEmpty('Bitte gib deinen Vornamen ein')]}
-                    onChange={loginDataChanged}/>
-                  <GridInputControl
-                    id='lastname'
-                    label='Nachname'
-                    xs={6}
-                    value={data.lastname}
-                    validators={[Validators.notEmpty('Bitte gib deinen Nachnamen ein')]}
-                    onChange={loginDataChanged}/>
-                  <GridInputControl
-                    id='telephone'
-                    label='Telefon'
-                    value={data.telephone}
-                    validators={[this.validateContact(Validators.telephone())]}
-                    onChange={loginDataChanged}/>
-                  <GridInputControl
-                    id='email'
-                    label='E-Mail'
-                    value={data.email}
-                    validators={[this.validateContact(Validators.email())]}
-                    onChange={loginDataChanged}/>
-                  <GridInputControl
-                    id='subject'
-                    label='Betreff'
-                    value={data.subject}
-                    validators={[Validators.notEmpty('Bitte gib einen Betreff an')]}
-                    onChange={loginDataChanged}/>
-                  <GridInputControl
-                    id='message'
-                    label='Nachricht'
-                    value={data.message}
-                    multiline
-                    validators={[Validators.notEmpty('Bitte trage hier deine Nachricht ein')]}
-                    onChange={loginDataChanged}/>
-                  {errorMessage ? <GridTextControl text={errorMessage} error={true}/> : undefined}
-                  <GridButtonControl
-                    pending={pending}
-                    label='absenden'
-                    onClick={this.doSendContact}/>
-                </ValidationGroup>
-              </Grid>
-            </CardContent>
-          </Collapse>
-        </Card>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
       </Grid>
     );
   }
@@ -183,7 +147,6 @@ const mapDispatchToProps = dispatch => ({
 
 
 export default compose(
-  withStyles(styles),
   connect(
     mapStateToProps,
     mapDispatchToProps
