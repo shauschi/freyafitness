e1b36abe88a6919ec051d83b3065edf6cdd17cf9
Stefan Hauschildt <stefan.h@uschildt.de>
2018-08-03 15:50:45 +0200

    - always map own profile in courses

--- src/main/java/freya/fitness/api/mapping/ProfileMapper.java
+++ src/main/java/freya/fitness/api/mapping/ProfileMapper.java
@@ -2,23 +2,14 @@ package freya.fitness.api.mapping;
 
 import freya.fitness.api.dto.ProfileDto;
 import freya.fitness.domain.jpa.User;
-import freya.fitness.domain.jpa.UserPreference;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProfileMapper {
 
   public ProfileDto map(final User user) {
-    return this.map(user, true);
-  }
-
-  public ProfileDto map(final User user, boolean showAll) {
     if (user == null) {
       return null;
-    }
-
-    if (!showAll && userWantsPrivacy(user)) {
-      return ProfileDto.Anonymous();
     }
 
     final ProfileDto dto = new ProfileDto();
@@ -28,15 +19,6 @@ public class ProfileMapper {
     dto.setEmail(user.getEmail());
 
     return dto;
-  }
-
-  private boolean userWantsPrivacy(final User user) {
-    return !user.getPreferences().stream()
-        .filter(userPreference -> UserPreference.VIEW_PARTICIPATION.equals(userPreference.getKey()))
-        .findFirst()
-        .map(UserPreference::getValue)
-        .filter("true"::equalsIgnoreCase)
-        .isPresent();
   }
 
 }
