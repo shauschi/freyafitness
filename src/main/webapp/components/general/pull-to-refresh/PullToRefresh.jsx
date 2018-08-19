e7a5b9cef0022df220c3d627fd9a94a71474f6b9
Stefan Hauschildt <stefan.h@uschildt.de>
2018-08-19 17:30:45 +0200

    - use different pull to refresh library (again)

--- src/main/webapp/components/general/pull-to-refresh/PullToRefresh.jsx
+++ src/main/webapp/components/general/pull-to-refresh/PullToRefresh.jsx
@@ -2,45 +2,52 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import ReactPullToRefresh from 'rmc-pull-to-refresh';
+import JsPullToRefresh from 'pulltorefreshjs';
 import Typography from '@material-ui/core/Typography';
 import {LoadingIndicator} from './../';
+import moment from 'moment';
+import './styles.css'
 
 class PullToRefresh extends Component {
+
+  state = {
+    updating: undefined
+  };
 
   static contextTypes = {
     onRefresh: PropTypes.func.isRequired,
     pending: PropTypes.bool.isRequired,
   };
 
-  onRefresh = () => {
+  onRefresh = cb => {
+    cb();
     this.props.onRefresh();
-    return new Promise(resolve => resolve());
+    this.setState({updating: true});
+    setTimeout(() => this.setState({updating: false}), 500);
   };
+
+  componentDidMount() {
+    JsPullToRefresh.init({
+      mainElement: '#pull-to-refresh',
+      triggerElement: '#pull-to-refresh',
+      refreshTimeout: 0,
+      instructionsPullToRefresh: 'zum Aktualisieren herunterziehen',
+      instructionsReleaseToRefresh: 'jetzt loslassen',
+      onRefresh: this.onRefresh
+    });
+  }
 
   render() {
     const {pending, style} = this.props;
+    const {updating} = this.state;
+    let loading = undefined;
+    if (pending || updating) {
+      loading = <LoadingIndicator noLabel style={{marginTop: '8px'}}/>;
+    }
     return (
-      <div style={{marginTop: '-16px', ...style}}>
-        <ReactPullToRefresh
-          style={{ height: 'calc(100vh - 96px)', overflow: 'auto' }}
-          indicator={{
-            deactivate:
-              <Typography variant='caption' style={{textAlign: 'center'}}>
-                zum Aktualisieren herunterziehen
-              </Typography>,
-            activate:
-              <Typography variant='caption' style={{textAlign: 'center'}}>
-                jetzt loslassen
-              </Typography>,
-            release: <LoadingIndicator noLabel/>,
-            finish: 'done'
-          }}
-          distanceToRefresh={48}
-          refreshing={pending}
-          onRefresh={this.onRefresh}
-        >
-          {this.props.children}
-        </ReactPullToRefresh>
+      <div style={style} id='pull-to-refresh'>
+        {loading}
+        {this.props.children}
       </div>
     )
   }
