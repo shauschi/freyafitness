9e39ea1d356fa7d625e37dafc0269b3c7574a94a
Stefan Hauschildt <stefan.h@uschildt.de>
2018-11-06 21:01:30 +0100

    - change method to post

--- src/main/java/freya/fitness/proxy/EmailProxy.java
+++ src/main/java/freya/fitness/proxy/EmailProxy.java
@@ -4,11 +4,17 @@ import freya.fitness.api.dto.MessageDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @FeignClient(name = "Email", url = "http://localhost:7700")
 public interface EmailProxy {
 
-  @RequestMapping("/emails")
+  @RequestMapping(
+      method = RequestMethod.POST,
+      value = "/emails",
+      consumes = "application/json",
+      produces = "application/json"
+  )
   MessageDto createEmail(@RequestBody final CreateEmail createEmail);
 
 }
