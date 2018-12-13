3321ba5112d532996630e1608706edf0f43633be
Stefan Hauschildt <stefan.h@uschildt.de>
2018-12-13 14:00:32 +0100

    - correction: change colors of course types

--- src/main/resources/db/changelog/009__course_type_colors/v0023__change_course_type_colors.sql
+++ src/main/resources/db/changelog/009__course_type_colors/v0023__change_course_type_colors.sql
@@ -1,2 +1,2 @@
-UPDATE public.course_type SET color = '#FFC107' WHERE name = 'BEST AGERS(65+)';
+UPDATE public.course_type SET color = '#00E676' WHERE name = 'FUN.TEAM';
 UPDATE public.course_type SET color = '#FF1493' WHERE name = 'MÄNNERABEND';
