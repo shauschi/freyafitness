5f64dd12dc11eee2006d3e9c0b65177bc919dffb
Stefan Hauschildt <stefan.h@uschildt.de>
2019-01-17 12:27:05 +0100

    - new course type open gym

---
+++ src/main/resources/db/changelog/012__add_course_type_open_gym/v0027__add_course_type_open_gym.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.course_type
+  (id, name, description, color, validity_from, validity_to)
+VALUES
+  ('9e88df32-2eed-427e-9db5-49b7f958c6af', 'OPEN.GYM', 'Training ohne Trainer. Der Trainingsplan wird gestellt, du entscheidest, ob du ihn machst oder dein ganz eigenes Training durchführst.', '#76FF03', '2019-01-17 00:00:00', '9999-12-31 23:59:59'),
