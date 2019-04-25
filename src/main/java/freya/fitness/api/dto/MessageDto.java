9b4f128e63b921c6b7255b5287ea4bdf53ef1063
Stefan Hauschildt <stefan.h@uschildt.de>
2019-04-25 08:14:59 +0200

    add zero args constructor for MessageDto

--- src/main/java/freya/fitness/api/dto/MessageDto.java
+++ src/main/java/freya/fitness/api/dto/MessageDto.java
@@ -2,8 +2,10 @@ package freya.fitness.api.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class MessageDto {
 
