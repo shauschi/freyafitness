5009efd404446cf57be724ffa837096d6aeda542
Stefan Hauschildt <stefan.h@uschildt.de>
2019-08-22 18:43:40 +0200

    add course type hyrox running

---
+++ src/main/resources/db/changelog/013__add_course_type_hyrox_running/v0027__add_course_type_open_gym.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.course_type
+  (id, name, description, color, validity_from, validity_to)
+VALUES
+  ('a9569410-c181-4a6e-a155-790ed73eb1af', 'LAUFTRAINING.HYROX', 'Vorbereitung für hyrox.', '#000000', '2019-08-22 00:00:00', '9999-12-31 23:59:59');
